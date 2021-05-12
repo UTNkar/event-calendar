0785507a749519bcd8c194939209fb472db310b0
Daniel Fehrm <daniel@fehrm.se>
2021-05-12 16:28:56 +0200

    configure media url and root

--- src/event_calendar/settings/base.py
+++ src/event_calendar/settings/base.py
@@ -107,6 +107,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "event_calendar.User"
