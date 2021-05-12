c1bca272e8b9b25466c485141eb0141871e393e2
Albin Antti <albin_antti@hotmail.com>
2021-05-12 16:08:08 +0200

    Change dev database engine to SQLite3
    Changes the database used in development to be SQLite, removing the
    need for an environment template, and moving the database engine
    selection from base to dev and production settings.

--- src/event_calendar/settings/production.py
+++ src/event_calendar/settings/production.py
@@ -7,6 +7,17 @@ DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET')
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DJANGO_DB_NAME', default='event_calendar'),
+        'USER': config('DJANGO_DB_USER', default='event_calendar'),
+        'PASSWORD': config('DJANGO_DB_PASS', default=''),
+        'HOST': config('DJANGO_DB_HOST', default='127.0.0.1'),
+        'PORT': config('DJANGO_DB_PORT', default='5432'),
+    }
+}
+
 ALLOWED_HOSTS = ['.utn.se']
 
 CSRF_COOKIE_SECURE = True
