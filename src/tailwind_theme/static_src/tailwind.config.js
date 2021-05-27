9da5e6cb64eea006b4c98f08ca42b28d8fefb07a
Daniel Fehrm <daniel@fehrm.se>
2021-05-27 17:07:59 +0200

    added header to list view

--- src/tailwind_theme/static_src/tailwind.config.js
+++ src/tailwind_theme/static_src/tailwind.config.js
@@ -51,7 +51,12 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'customgreen': '#00EB7A',
+                'customblue': '#007EDA',
+            }
+        },
     },
     variants: {
         extend: {},
